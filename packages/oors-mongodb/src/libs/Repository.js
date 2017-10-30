bb338b2d2a54423af6e80cdddf739dc75de3d1bf
Victor Zamfir <zamfir.victor@gmail.com>
2017-10-30 14:45:52 +0200

    [oors-mongodb] Repository improvements
    - adding stamps
    - better schema detection

--- packages/oors-mongodb/src/libs/Repository.js
+++ packages/oors-mongodb/src/libs/Repository.js
@@ -1,21 +1,62 @@
+/* eslint-disable no-underscore-dangle */
 import Joi from 'joi';
 import { CRUDServiceContainer } from 'octobus-crud';
 
 class Repository extends CRUDServiceContainer {
-  constructor(schema, collectionName) {
-    super({
-      _id: Joi.object(),
-      ...schema,
-    });
+  stamps = {
+    user: true,
+    time: true,
+  };
+
+  constructor(...args) {
+    super(...args);
+
+    if (!this.schema) {
+      this.schema = this.constructor.schema;
+    }
 
     if (!this.collectionName) {
-      this.collectionName =
-        collectionName ||
-        this.constructor.name.substr(
-          0,
-          this.constructor.name.indexOf('Repository'),
-        );
+      this.collectionName = this.constructor.name.substr(
+        0,
+        this.constructor.name.indexOf('Repository'),
+      );
     }
+  }
+
+  set schema(schema) {
+    this._schema = schema;
+    this.addIdToSchema();
+    this.addStamps();
+  }
+
+  get schema() {
+    return this._schema;
+  }
+
+  addStamps() {
+    if (this.stamps.user) {
+      Object.assign(this.schema, {
+        createdBy: Joi.object(),
+        updatedBy: Joi.object(),
+      });
+    }
+
+    if (this.stamps.time) {
+      Object.assign(this.schema, {
+        createdAt: Joi.date(),
+        updatedAt: Joi.date(),
+      });
+    }
+  }
+
+  addIdToSchema() {
+    if (this.schema._id) {
+      return;
+    }
+
+    Object.assign(this.schema, {
+      _id: Joi.object(),
+    });
   }
 }
 
