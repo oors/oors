294280c36535a4b1e8ca3da73512c904d8639458
Victor Zamfir <zamfir.victor@gmail.com>
2017-11-03 01:46:30 +0200

    [oors-graphQL] withArgs decorator promise resolving support

--- packages/oors-graphql/src/libs/decorators.js
+++ packages/oors-graphql/src/libs/decorators.js
@@ -1,5 +1,6 @@
 import flow from 'lodash/flow';
 import ValidationError from 'oors/build/errors/ValidationError';
+import isPlainObject from 'lodash/isPlainObject';
 
 export const withSchema = schema => {
   let validate;
@@ -17,8 +18,40 @@ export const withSchema = schema => {
   };
 };
 
-export const withArgs = parser => resolver => async (_, args, ctx, info) =>
-  resolver(_, await parser(args, ctx), ctx, info);
+const replaceValues = (obj, values, replacers) => {
+  Object.keys(obj).forEach(key => {
+    const value = obj[key];
+
+    if (isPlainObject(value)) {
+      replaceValues(value, values, replacers);
+    } else {
+      const index = values.indexOf(value);
+      if (index > -1) {
+        obj[key] = replacers[index]; // eslint-disable-line no-param-reassign
+      }
+    }
+  });
+};
+
+export const withArgs = parser => resolver => async (_, args, ctx, info) => {
+  const keys = [];
+  const promises = [];
+
+  const finalArgs = await parser(args, ctx, {
+    resolve: promise => {
+      const key = Symbol(keys.index);
+      keys.push(key);
+      promises.push(promise);
+      return key;
+    },
+  });
+
+  const values = await Promise.all(promises);
+
+  replaceValues(finalArgs, keys, values);
+
+  return resolver(_, finalArgs, ctx, info);
+};
 
 export const withContext = fn => resolver => async (_, args, ctx, info) =>
   resolver(_, args, await fn(ctx), info);
