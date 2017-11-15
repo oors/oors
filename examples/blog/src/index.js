11efa9d63fbfd4e319daf4149a8d72e04cddeb0a
Victor Zamfir <zamfir.victor@gmail.com>
2017-11-15 12:37:17 +0200

    blog example - default graphiql query

--- examples/blog/src/index.js
+++ examples/blog/src/index.js
@@ -1,9 +1,28 @@
 /* eslint-disable no-console */
 import './env';
+import config from '../../../packages/oors-presets/src/config';
 import Application from '../../../packages/oors-presets/src/applications/Standard';
 import BlogModule from './modules/Blog';
 
-const app = new Application();
+config.set(
+  'modules.oors.graphQL.graphiql.params.query',
+  `
+{
+  blogPosts {
+    id
+    title
+    category {
+      id
+      name
+    }
+    comments {
+      body
+    }
+  }
+}`,
+);
+
+const app = new Application(config);
 
 app.addModules(new BlogModule());
 
