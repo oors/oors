1575462bbca046e76c349c15d3f33c4046190099
Victor Zamfir <zamfir.victor@gmail.com>
2018-03-12 19:04:49 +0200

    better error handler

--- packages/oors-presets/src/middlewares/errorHandler.js
+++ packages/oors-presets/src/middlewares/errorHandler.js
@@ -1,19 +1,18 @@
-import errorHandlerFactory from 'errorhandler';
-
-const errorHandler = errorHandlerFactory();
+import omit from 'lodash/omit';
 
 export default {
   id: 'errorHandler',
+  // eslint-disable-next-line no-unused-vars
   factory: ({ isDev }) => (err, req, res, next) => {
-    if (isDev) {
-      return errorHandler(err, req, res, next);
+    const status = err.statusCode || err.status || 500;
+    const error = isDev ? err : omit(err, 'stack');
+
+    if (!isDev) {
+      req.app.modules.get('oors.logger').logger.error(err);
     }
 
-    req.app.modules.get('oors.logger').logger.error(err);
-    res.status(500);
-
-    return res.json({
-      message: 'Internal server error!',
+    return res.status(status).json({
+      error,
     });
   },
 };
