38f66d062aea265927ded7ea97ede07ca48ec934
Victor Zamfir <zamfir.victor@gmail.com>
2018-04-27 12:50:40 +0300

    [oors-graphql] Date scalar

--- packages/oors-graphql/src/graphql/resolvers.js
+++ packages/oors-graphql/src/graphql/resolvers.js
@@ -1,3 +1,4 @@
+import padStart from 'lodash/padStart';
 import { GraphQLScalarType } from 'graphql';
 import { Kind } from 'graphql/language';
 import GraphQLJSON from 'graphql-type-json';
@@ -27,6 +28,37 @@ export default {
     serialize: value => {
       const date = value instanceof Date ? value : new Date(value);
       return date.toISOString();
+    },
+  }),
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue: value => new Date(value),
+    parseLiteral(ast) {
+      if (ast.kind !== Kind.STRING || !ast.value.match(/^\d{4}-\d{2}-\d{2}$/)) {
+        throw new Error('Invalid date format');
+      }
+
+      const date = new Date(ast.value);
+
+      if (date.toString() === 'Invalid Date') {
+        throw new Error('Invalid Date');
+      }
+
+      return date;
+    },
+    serialize: value => {
+      const date = value instanceof Date ? value : new Date(value);
+
+      if (date.toString() === 'Invalid Date') {
+        throw new Error('Invalid Date');
+      }
+
+      return [
+        date.getFullYear(),
+        padStart(date.getMonth() + 1, 2, 0),
+        padStart(date.getDate(), 2, 0),
+      ].join('-');
     },
   }),
   JSON: GraphQLJSON,
