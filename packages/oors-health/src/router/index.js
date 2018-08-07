5fba39072429f82ccb1234b280f585661f055734
Victor Zamfir <zamfir.victor@gmail.com>
2018-08-07 14:09:25 +0300

    [oors-health] readable startTime and upTime

--- packages/oors-health/src/router/index.js
+++ packages/oors-health/src/router/index.js
@@ -1,12 +1,22 @@
 import { Router } from 'express';
+import ms from 'ms';
 
 const router = Router();
 
 router.get('/health', (req, res) => {
+  const startTime = req.app.get('startTime');
+  const upTime = Date.now() - req.app.get('startTime');
+
   res.json({
-    startTime: req.app.get('startTime'),
-    upTime: Date.now() - req.app.get('startTime'),
+    startTime,
+    upTime,
     memoryUsage: process.memoryUsage(),
+    readable: {
+      startTime: ms(startTime, { long: true }),
+      upTime: ms(upTime, {
+        long: true,
+      }),
+    },
   });
 });
 
