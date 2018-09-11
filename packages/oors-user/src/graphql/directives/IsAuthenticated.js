77df015a8d65fb3b4ba61f1432071de62f70e266
Victor Zamfir <zamfir.victor@gmail.com>
2018-09-11 23:55:24 +0300

    [oors-user] IsAuthenticatedDirective accepts a list of roles

--- packages/oors-user/src/graphql/directives/IsAuthenticated.js
+++ packages/oors-user/src/graphql/directives/IsAuthenticated.js
@@ -1,5 +1,5 @@
 /* eslint-disable class-methods-use-this, no-underscore-dangle */
-import { SchemaDirectiveVisitor, AuthenticationError } from 'apollo-server';
+import { SchemaDirectiveVisitor, AuthenticationError, ForbiddenError } from 'apollo-server';
 import { /* DirectiveLocation, GraphQLDirective, */ defaultFieldResolver } from 'graphql';
 
 class IsAuthenticatedDirective extends SchemaDirectiveVisitor {
@@ -14,19 +14,33 @@ class IsAuthenticatedDirective extends SchemaDirectiveVisitor {
     const fields = type.getFields();
     Object.keys(fields).forEach(fieldName => {
       const field = fields[fieldName];
+
+      if (!field.isAuthenticatedConfig) {
+        field.isAuthenticatedConfig = {};
+      }
+
+      field.isAuthenticatedConfig.typeRoles = this.args.roles;
+
       this.wrapField(field);
     });
   }
 
   visitFieldDefinition(field) {
+    if (!field.isAuthenticatedConfig) {
+      field.isAuthenticatedConfig = {}; // eslint-disable-line no-param-reassign
+    }
+
+    field.isAuthenticatedConfig.roles = this.args.roles; // eslint-disable-line no-param-reassign
     this.wrapField(field);
   }
 
   wrapField(field) {
-    if (field.isAuthenticatedWrapped) {
+    const { isAuthenticatedConfig } = field;
+    if (isAuthenticatedConfig.isWrapped) {
       return;
     }
 
+    isAuthenticatedConfig.isWrapped = true;
     const { resolve = defaultFieldResolver } = field;
 
     Object.assign(field, {
@@ -34,6 +48,15 @@ class IsAuthenticatedDirective extends SchemaDirectiveVisitor {
       resolve: async (root, args, context, info) => {
         if (!context.user) {
           throw new AuthenticationError('Not authenticated!');
+        }
+
+        const requiredRoles = isAuthenticatedConfig.roles || isAuthenticatedConfig.typeRoles;
+        if (Array.isArray(requiredRoles) && requiredRoles.length) {
+          requiredRoles.forEach(role => {
+            if (!(context.user.roles || []).includes(role)) {
+              throw new ForbiddenError(`Restricted access! Missing "${role}" role!`);
+            }
+          });
         }
 
         return resolve.call(this, root, args, context, info);
