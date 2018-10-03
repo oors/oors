75f6ff3952ce0aaee2eddc15acdd759825b20a82
Victor Zamfir <zamfir.victor@gmail.com>
2018-10-03 15:18:44 +0300

    [oors-graphql] definition of @cost directive

--- packages/oors-graphql/src/libs/Server.js
+++ packages/oors-graphql/src/libs/Server.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from 'apollo-server-express';
 import costAnalysis from 'graphql-cost-analysis';
+import { ValidationError } from 'apollo-server';
 
 class Server extends ApolloServer {
   constructor({ costAnalysisConfig, ...config }) {
@@ -13,7 +14,14 @@ class Server extends ApolloServer {
       ...options,
       validationRules: [
         ...options.validationRules,
-        costAnalysis({ variables: req.body.variables, ...this.costAnalysisConfig }),
+        costAnalysis({
+          variables: req.body.variables,
+          createError: (max, actual) =>
+            new ValidationError(
+              `GraphQL query exceeds maximum complexity, please remove some nesting or fields and try again. (max: ${max}, actual: ${actual})`,
+            ),
+          ...this.costAnalysisConfig,
+        }),
       ],
     };
   }
