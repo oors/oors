f6a53d0d0783a1656f1e698e28a3dde8d33c1aa4
Victor Zamfir <zamfir.victor@gmail.com>
2018-10-22 23:53:29 +0300

    blog example changes

--- examples/blog/src/index.js
+++ examples/blog/src/index.js
@@ -2,11 +2,9 @@
 import Application from '../../../packages/oors-presets/build/applications/Standard';
 import config from '../../../packages/oors-presets/build/config';
 import BlogModule from './modules/Blog';
-import RADModule from '../../../packages/oors-rad/build/index';
 
 const app = new Application(config);
 
 app.addModules(new BlogModule());
-app.addModules(new RADModule());
 
 app.listen().catch(console.log.bind(console));
