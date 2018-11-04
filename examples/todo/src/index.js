0ad1a20b5466e811ed36c31697285a33f102236a
Victor Zamfir <zamfir.victor@gmail.com>
2018-11-04 16:09:43 +0200

    remove todo example

--- examples/todo/src/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-import Application from '../../../packages/oors-presets/src/applications/Standard';
-import TodoModule from './modules/Todo';
-
-const app = new Application();
-
-app.addModules(new TodoModule());
-
-app.listen().catch(console.log.bind(console));
