095236dfe659a4e0f4209ad148cfbaf4c312a9ad
Victor Zamfir <zamfir.victor@gmail.com>
2018-11-08 21:43:44 +0200

    [oors-graphql] withDefaultArgs can accept a function

--- packages/oors-graphql/src/decorators/withDefaultArgs.js
+++ packages/oors-graphql/src/decorators/withDefaultArgs.js
@@ -1,4 +1,8 @@
 import merge from 'lodash/merge';
 
-export default defaultArgs => resolver => async (_, args, ctx, info) =>
-  resolver(_, merge({}, defaultArgs, args), ctx, info);
+export default defaultArgs => resolver => async (_, args, ctx, info) => {
+  const defaults =
+    typeof defaultArgs === 'function' ? defaultArgs(_, args, ctx, info) : defaultArgs;
+
+  return resolver(_, merge({}, defaults, args), ctx, info);
+};
