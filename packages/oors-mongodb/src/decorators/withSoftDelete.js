9473d6115c0f0eb29cfd8b30979bfcd64d99f092
Victor Zamfir <zamfir.victor@gmail.com>
2018-11-14 11:55:32 +0200

    [oors-mongodb] improved softDelete decorator

--- packages/oors-mongodb/src/decorators/withSoftDelete.js
+++ packages/oors-mongodb/src/decorators/withSoftDelete.js
@@ -1,7 +1,25 @@
 import set from 'lodash/set';
 
+const toSoftDeleteFilter = ({ noSoftDeleteFilter, propertyName }) =>
+  noSoftDeleteFilter
+    ? {}
+    : {
+        [propertyName]: false,
+      };
+
 export default ({ propertyName = 'isDeleted' } = {}) => repository => {
-  const { findOne, findMany, count, updateOne, updateMany } = repository;
+  const {
+    findOne,
+    findMany,
+    count,
+    updateOne,
+    updateMany,
+    deleteOne,
+    deleteMany,
+    toMongoPipeline,
+    aggregate,
+    createPipeline,
+  } = repository;
 
   set(repository, `schema.properties.${propertyName}`, {
     type: 'boolean',
@@ -9,48 +27,88 @@ export default ({ propertyName = 'isDeleted' } = {}) => repository => {
   });
 
   Object.assign(repository, {
-    findOne: ({ query = {}, ...restArgs } = {}) =>
+    findOne: ({ query = {}, noSoftDeleteFilter, ...restArgs } = {}) =>
       findOne.call(repository, {
         query: {
-          isDeleted: false,
+          ...toSoftDeleteFilter({ propertyName, noSoftDeleteFilter }),
           ...query,
         },
         ...restArgs,
       }),
-    findMany: ({ query = {}, ...restArgs } = {}) =>
+    findMany: ({ query = {}, noSoftDeleteFilter, ...restArgs } = {}) =>
       findMany.call(repository, {
         query: {
-          isDeleted: false,
+          ...toSoftDeleteFilter({ propertyName, noSoftDeleteFilter }),
           ...query,
         },
         ...restArgs,
       }),
-    count: ({ query = {}, ...restArgs } = {}) =>
+    count: ({ query = {}, noSoftDeleteFilter, ...restArgs } = {}) =>
       count.call(repository, {
         query: {
-          isDeleted: false,
+          ...toSoftDeleteFilter({ propertyName, noSoftDeleteFilter }),
           ...query,
         },
         ...restArgs,
       }),
-    deleteOne: ({ query = {} } = {}) =>
-      updateOne.call(repository, {
-        query,
-        update: {
-          $set: {
-            [propertyName]: true,
-          },
-        },
-      }),
-    deleteMany: ({ query = {} } = {}) =>
-      updateMany.call(repository, {
-        query,
-        update: {
-          $set: {
-            [propertyName]: true,
-          },
-        },
-      }),
+    deleteOne: ({ query = {}, noSoftDeleteFilter, ...restArgs } = {}) =>
+      noSoftDeleteFilter
+        ? deleteOne.call(repository, {
+            query,
+            ...restArgs,
+          })
+        : updateOne.call(repository, {
+            query,
+            update: {
+              $set: {
+                [propertyName]: true,
+              },
+            },
+          }),
+    deleteMany: ({ query = {}, noSoftDeleteFilter, ...restArgs } = {}) =>
+      noSoftDeleteFilter
+        ? deleteMany.call(repository, {
+            query,
+            ...restArgs,
+          })
+        : updateMany.call(repository, {
+            query,
+            update: {
+              $set: {
+                [propertyName]: true,
+              },
+            },
+          }),
+    aggregate: (pipeline, { noSoftDeleteFilter, ...restOptions } = {}) =>
+      aggregate.call(
+        repository,
+        noSoftDeleteFilter
+          ? pipeline
+          : [
+              {
+                $match: {
+                  [propertyName]: false,
+                },
+              },
+              ...toMongoPipeline.call(repository, pipeline),
+            ],
+        restOptions,
+      ),
+    createPipeline: (initialPipeline = [], { noSoftDeleteFilter, ...restOptions }) =>
+      createPipeline.call(
+        repository,
+        noSoftDeleteFilter
+          ? initialPipeline
+          : [
+              {
+                $match: {
+                  [propertyName]: false,
+                },
+              },
+              ...initialPipeline,
+            ],
+        restOptions,
+      ),
   });
 
   return repository;
