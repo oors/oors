795a49b8f68f439ca58c95371352f5e8dd2f9f12
Victor Zamfir <zamfir.victor@gmail.com>
2018-11-16 12:36:00 +0200

    blog example fixes

--- examples/blog/src/modules/Blog/index.js
+++ examples/blog/src/modules/Blog/index.js
@@ -1,7 +1,14 @@
 import { Module } from '../../../../../packages/oors';
+import withSoftDelete from '../../../../../packages/oors-mongodb/build/decorators/withSoftDelete';
 
 class BlogModule extends Module {
   name = 'oors.blog';
+
+  async setup() {
+    await this.dependencies(['oors.mongodb']);
+    const { Post } = this.get('repositories');
+    withSoftDelete()(Post);
+  }
 }
 
 export default BlogModule;
