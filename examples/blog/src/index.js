212d365b07a8c31568a966175519f4cee26a22a5
Victor Zamfir <zamfir.victor@gmail.com>
2018-12-10 16:23:32 +0200

    blog example - oors.web integration

--- examples/blog/src/index.js
+++ examples/blog/src/index.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-console */
+import path from 'path';
 import Application from '../../../packages/oors-presets/build/applications/Standard';
 import config from '../../../packages/oors-presets/build/config';
 // import AJVModule from '../../../packages/oors-ajv/build/index';
+import WebModule from '../../../packages/oors-web';
 import BlogModule from './modules/Blog';
 
+config.set('rootDir', path.resolve(__dirname, '..'));
 config.set('modules.oors.mongodb.autoloadRepositories', true);
+config.set(
+  'modules.oors.web.nextMiddlewareConfig.params.appConfig.dir',
+  path.resolve(config.get('rootDir'), 'web'),
+);
 
 const app = new Application(config);
 
-app.addModules(new BlogModule());
+app.addModules(new BlogModule(), new WebModule());
 
 app.listen().catch(console.log.bind(console));
---
+++ examples/blog/web/pages/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div>it works!</div>;
