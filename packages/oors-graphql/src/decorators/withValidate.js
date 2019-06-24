9dd051a6863d23b21e0f60000d491ddab7690971
Victor Zamfir <zamfir.victor@gmail.com>
2019-06-24 12:34:00 +0300

    [oors-graphql] withValidate decorator improvements

--- packages/oors-graphql/src/decorators/withValidate.js
+++ packages/oors-graphql/src/decorators/withValidate.js
@@ -1,9 +1,14 @@
 import { UserInputError } from 'apollo-server';
 
-export default validate => resolver => (_, args, ctx, info) => {
-  if (!validate(_, args, ctx, info)) {
-    throw new UserInputError('Failed validation!', {
-      errorMessages: validate.errors,
+export default validate => resolver => async (_, args, ctx, info) => {
+  try {
+    const isValid = await validate(_, args, ctx, info);
+    if (!isValid) {
+      throw new Error();
+    }
+  } catch (error) {
+    throw new UserInputError(error.message || 'Failed Validation!', {
+      originalError: error,
     });
   }
 
