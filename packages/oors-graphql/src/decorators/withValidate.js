d6ee1a33dbdc90cc2e292675381bd345e2cf3fce
Victor Zamfir <zamfir.victor@gmail.com>
2019-06-24 12:38:19 +0300

    [oors-graphql] withValidate accepts a custom error message

--- packages/oors-graphql/src/decorators/withValidate.js
+++ packages/oors-graphql/src/decorators/withValidate.js
@@ -1,13 +1,13 @@
 import { UserInputError } from 'apollo-server';
 
-export default validate => resolver => async (_, args, ctx, info) => {
+export default (validate, message) => resolver => async (_, args, ctx, info) => {
   try {
     const isValid = await validate(_, args, ctx, info);
     if (!isValid) {
       throw new Error();
     }
   } catch (error) {
-    throw new UserInputError(error.message || 'Failed Validation!', {
+    throw new UserInputError(message || error.message || 'Failed Validation!', {
       originalError: error,
     });
   }
